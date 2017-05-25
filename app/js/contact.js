83326129874ef3d36acbb9e04b2579cb145582f5
Daniel Danielecki <daniel.danielecki@mail.com>
2017-05-25 16:24:43 +0200

    add validation

--- app/js/contact.js
+++ app/js/contact.js
@@ -1,34 +1,34 @@
 // Define angular module/app.
-var formApp = angular.module('contact', []);
+var formApp = angular.module('contactApp', []);
 
 // Create angular controller and pass in $scope and $http.
 var controller = formApp.controller('contactForm', ['$scope', '$http', function ($scope, $http) {
-  $scope.input = {};
+  $scope.formData = {};
 
   // Process the form.
   $scope.processForm = function () {
     $http({
       method  : 'POST',
-      url     : 'php/contact-form.php',
-      data    : angular.element.param($scope.formData), // Pass in data as strings.
+      url     : 'contact-form.php',
+      data    : $scope.formData, // Angular will take care about serializing.
       headers : {'Content-Type': 'application/x-www-form-urlencoded'} // Set the headers so angular passing info as form data (not request payload).
     })
 
-      .success(function (data) {
-        console.log(data);
-
-        if (!data.success) {
-          // If not successful, bind errors to error variables.
-          $scope.errorName = data.errors.name;
-          $scope.errorSubject = data.errors.subject;
-          $scope.errorTelephone = data.errors.telephone;
-          $scope.errorEmail = data.errors.email;
-          $scope.errorMessage = data.errors.message;
-        } else {
-          // If successful, bind success message to message
-          $scope.message = data.message;
-        }
-      });
+      // .success(function (data) {
+      //   console.log(data);
+      //
+      //   if (!data.success) {
+      //     // If not successful, bind errors to error variables.
+      //     $scope.errorName = data.errors.name;
+      //     $scope.errorSubject = data.errors.subject;
+      //     $scope.errorTelephone = data.errors.telephone;
+      //     $scope.errorEmail = data.errors.email;
+      //     $scope.errorMessage = data.errors.message;
+      //   } else {
+      //     // If successful, bind success message to message
+      //     $scope.message = data.message;
+      //   }
+      // });
   };
 }]);
 
